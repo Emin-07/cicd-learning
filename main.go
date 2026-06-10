c0f4da6986c9d9010d057715ce4aa010493700a3
Mohmad Saidov <moxmad_saidov@mail.ru>
2026-06-10 15:27:31 +0300

    prev race condition i added was fixed in version 1.22, changed to new race condition'

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"sync"
 )
 
+var x int
+
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(5)
 
-	for i := 0; i < 5; i++ {
+	for ; x < 5; x++ {
 		go func() {
-			fmt.Println(i)
+			fmt.Println(x)
 			wg.Done()
 		}()
 	}
